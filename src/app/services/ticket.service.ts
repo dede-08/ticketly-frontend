768b8087a6de2738bc963db602791f44e10536b9
dede-08 <benandrealanyaguevara@gmail.com>
2025-11-05 18:38:32 -0500

    updates

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { env } from '../../app/environments/.env';
+import { environment as env } from '../../environments/environment';
 
 export interface User {
   id: number;
